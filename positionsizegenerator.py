8abbc02757365e41b227b03da90be85cb7c1b8f2
MirbahaMoein <mirbahamoein98@gmail.com>
2022-10-29 09:35:19 +0330

    Feat: Position Size Calculation

---
+++ positionsizegenerator.py
@@ -0,0 +1,24 @@
+import pyperclip
+
+while True:
+    try:
+        stoplossinput = input("Enter stop loss price: ")
+        stoploss = float(stoplossinput)
+        while True:
+            try:
+                entryinput = input("Enter entry price: ")
+                entry = float(entryinput)
+                targetinput = input("Enter target price: ")
+                target = float(targetinput)
+                riskpercentageinput = input("Enter desired risk percentage(e.g. '0.5'): ")
+                riskpercentage = float(riskpercentageinput)
+                slpercentage = abs(((entry - stoploss) / stoploss) * 100)
+                positionsizeusd = 100000 * riskpercentage / slpercentage
+                positionsizesymbol = positionsizeusd / entry
+                outputstring = str(int(positionsizesymbol*100)/100)
+                pyperclip.copy(outputstring)
+                print(outputstring, '   (Copied to clipboard!)')
+            except:
+                pass
+    except:
+        pass
